80a0f7f7690d1d14cfa90d3355fbd37ef6e8e415
opensignature <antiac@gmail.com>
2022-02-02 18:26:16 +0100

    Add files via upload

---
+++ Util/subdir.mk
@@ -0,0 +1,41 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+
+CPP_SRCS += \
+Array.cpp \
+CacheLib.cpp \
+CryptoppUtils.cpp \
+IniSettings.cpp \
+ModuleInfo.cpp \
+SyncroEvent.cpp \
+SyncroMutex.cpp \
+TLV.cpp \
+UUCByteArray.cpp \
+UUCProperties.cpp \
+UUCStringTable.cpp \
+UUCTextFileReader.cpp \
+UtilException.cpp \
+funccallinfo.cpp \
+log.cpp \
+util.cpp 
+
+OBJS += \
+./Util/Array.o \
+./Util/CacheLib.o \
+./Util/CryptoppUtils.o \
+./Util/IniSettings.o \
+./Util/ModuleInfo.o \
+./Util/SyncroEvent.o \
+./Util/SyncroMutex.o \
+./Util/TLV.o \
+./Util/UUCByteArray.o \
+./Util/UUCProperties.o \
+./Util/UUCStringTable.o \
+./Util/UUCTextFileReader.o \
+./Util/UtilException.o \
+./Util/funccallinfo.o \
+./Util/log.o \
+./Util/util.o 
